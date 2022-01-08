d40dc4c7b7444a96644cc4a6a42ec17b59161098
LisandroPer <lisanperri95@hotmail.com>
2022-01-08 17:53:15 -0300

    El carousel ahora puede pasar imagenes. Falta resolver el problema de la visualización en pantallas grandews, en celulares está funcionando perfecto

--- src/components/ItemCarousel.jsx
+++ src/components/ItemCarousel.jsx
@@ -3,14 +3,32 @@
 export default function ItemCarousel( {casa} ){
     return(
         <>
-          <div class="card bg-dark text-white">
-            <img src={casa.carouselImg.primary} class="card-img" alt="..." />
-            <div class="card-img-overlay">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text">Last updated 3 mins ago</p>
+          <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src={casa.primary} class="d-block w-100 imgcarusel" alt="..."  />
+                </div>
+                <div class="carousel-item">
+                <img src={casa.secundary} class="d-block w-100 imgcarusel" alt="..."  />
+                </div>
+                <div class="carousel-item">
+                <img src={casa.third} class="d-block w-100 imgcarusel" alt="..."   />
+                </div>
             </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
         </>
     );
 }
--- src/utils/dataDuplex.js
+++ src/utils/dataDuplex.js
@@ -1,15 +1,15 @@
 const duplex = [
     {id: 1, duplexName: "Duplex 3" , capacity: 5, mainImg: "https://images.sparairnos.com/property/22923/images/364183/full/e4ed200c-bb4d-40ca-83d8-7e07f5029fdf", 
-    carouselImg: {primary:"https://images.unsplash.com/photo-1604517900654-63cf1a0dabf3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", secundary: "https://images.unsplash.com/photo-1603306604810-22ecdf5c6228?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80", 
-    third: "https://images.unsplash.com/photo-1586713400549-4df5d11f2a29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}},
+    primary:"https://images.unsplash.com/photo-1604517900654-63cf1a0dabf3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", secundary: "https://images.unsplash.com/photo-1603306604810-22ecdf5c6228?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80", 
+    third: "https://images.unsplash.com/photo-1586713400549-4df5d11f2a29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"},
     
     {id: 2, duplexName: "Duplex 4", capacity: 5, mainImg: "https://images.sparairnos.com/property/22923/images/364183/full/d32b77a4-cdc8-4176-90fc-aa6e6966bd25", 
-    carouselImg: {primary:"https://images.unsplash.com/photo-1604517900654-63cf1a0dabf3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", secundary: "https://images.unsplash.com/photo-1603306604810-22ecdf5c6228?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80", 
-    third: "https://images.unsplash.com/photo-1586713400549-4df5d11f2a29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}},
+    primary:"https://images.unsplash.com/photo-1604517900654-63cf1a0dabf3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", secundary: "https://images.unsplash.com/photo-1603306604810-22ecdf5c6228?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80", 
+    third: "https://images.unsplash.com/photo-1586713400549-4df5d11f2a29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"},
     
     {id: 3, duplexName: "Duplex 5", capacity: 5, mainImg: "https://images.sparairnos.com/property/22923/images/364183/full/ac2db5d9-dcc8-45dd-b6f3-c9d913c495af", 
-    carouselImg: {primary:"https://images.unsplash.com/photo-1604517900654-63cf1a0dabf3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", secundary: "https://images.unsplash.com/photo-1603306604810-22ecdf5c6228?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80", 
-    third: "https://images.unsplash.com/photo-1586713400549-4df5d11f2a29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}}
+    primary:"https://images.unsplash.com/photo-1604517900654-63cf1a0dabf3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", secundary: "https://images.unsplash.com/photo-1603306604810-22ecdf5c6228?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80", 
+    third: "https://images.unsplash.com/photo-1586713400549-4df5d11f2a29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}
 ]
 
 
